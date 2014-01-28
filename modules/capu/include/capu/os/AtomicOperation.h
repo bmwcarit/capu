a86b67739d8a3110af46acbae711b168e6c6d3e4
Aaron Frueh <aaron.frueh@partner.bmw.de>
2014-01-28 18:55:04 +0100

    AtomicOperations Interface rework for 64-Bit SmartPointer - Changed AtomicOperations to a general interface
    Embedded 64 Bit Architectures had issues with the 32 Bit Counter-Pointer, which lead to stack canaries exceptions.

--- modules/capu/include/capu/os/AtomicOperation.h
+++ modules/capu/include/capu/os/AtomicOperation.h
@@ -30,65 +30,242 @@ namespace capu
     class AtomicOperation: private os::arch::AtomicOperation
     {
     public:
-
         /**
          * atomically add 'summand' to an uint32_t
          * @param mem reference to the object
          * @param summand amount to add
          * @return returns the initial value of mem
          */
-        static uint32_t AtomicAdd32(volatile uint32_t& mem, uint32_t summand);
+        static uint32_t AtomicAdd(volatile uint32_t& mem, uint32_t summand);
 
         /**
-         * atomically subtract 'substrahend' from an uint32_t
+         * atomically add 'summand' to an int32_t
+         * @param mem reference to the object
+         * @param summand amount to add
+         * @return returns the initial value of mem
+         */
+        static int32_t AtomicAdd(volatile int32_t& mem, int32_t summand);
+
+        /**
+         * atomically add 'summand' to an uint64_t
+         * @param mem reference to the object
+         * @param summand amount to add
+         * @return returns the initial value of mem
+         */
+        static uint64_t AtomicAdd(volatile uint64_t& mem, uint64_t summand);
+
+        /**
+         * atomically add 'summand' to an int64_t
+         * @param mem reference to the object
+         * @param summand amount to add
+         * @return returns the initial value of mem
+         */
+        static int64_t AtomicAdd(volatile int64_t& mem, int64_t summand);
+
+
+        /**
+         * atomically subtract 'substrahend' to an uint32_t
          * @param mem reference to the object
          * @param subtrahend amount to subtract
          * @return returns the initial value of mem
          */
-        static uint32_t AtomicSub32(volatile uint32_t& mem, uint32_t subtrahend);
+        static uint32_t AtomicSub(volatile uint32_t& mem, uint32_t subtrahend);
+
+        /**
+         * atomically subtract 'substrahend' to an int32_t
+         * @param mem reference to the object
+         * @param subtrahend amount to subtract
+         * @return returns the initial value of mem
+         */
+        static int32_t AtomicSub(volatile int32_t& mem, int32_t subtrahend);
+
+        /**
+         * atomically subtract 'substrahend' to an uint64_t
+         * @param mem reference to the object
+         * @param subtrahend amount to subtract
+         * @return returns the initial value of mem
+         */
+        static uint64_t AtomicSub(volatile uint64_t& mem, uint64_t subtrahend);
+
+        /**
+         * atomically subtract 'substrahend' to an int64_t
+         * @param mem reference to the object
+         * @param subtrahend amount to subtract
+         * @return returns the initial value of mem
+         */
+        static int64_t AtomicSub(volatile int64_t& mem, int64_t subtrahend);
 
         /**
          * atomically increment an uint32_t
          * @param mem reference to the object
          * @return returns the initial value of mem
          */
-        static uint32_t AtomicInc32(volatile uint32_t& mem);
+        static uint32_t AtomicInc(volatile uint32_t& mem);
 
         /**
-         * atomically decrement  an uint32_t
+         * atomically increment an int32_t
          * @param mem reference to the object
          * @return returns the initial value of mem
          */
-        static uint32_t AtomicDec32(volatile uint32_t& mem);
+        static int32_t  AtomicInc(volatile int32_t& mem);
+
+        /**
+         * atomically increment an uint64_t
+         * @param mem reference to the object
+         * @return returns the initial value of mem
+         */
+        static uint64_t AtomicInc(volatile uint64_t& mem);
+
+        /**
+         * atomically increment an int64_t
+         * @param mem reference to the object
+         * @return returns the initial value of mem
+         */
+        static int64_t  AtomicInc(volatile int64_t& mem);
+
+        /**
+         * atomically decrement an uint32_t
+         * @param mem reference to the object
+         * @return returns the initial value of mem
+         */
+        static uint32_t AtomicDec(volatile uint32_t& mem);
+
+        /**
+         * atomically decrement an int32_t
+         * @param mem reference to the object
+         * @return returns the initial value of mem
+         */
+        static int32_t  AtomicDec(volatile int32_t& mem);
+
+        /**
+         * atomically decrement an uint64_t
+         * @param mem reference to the object
+         * @return returns the initial value of mem
+         */
+        static uint64_t AtomicDec(volatile uint64_t& mem);
+
+        /**
+         * atomically decrement an int64_t
+         * @param mem reference to the object
+         * @return returns the initial value of mem
+         */
+        static int64_t  AtomicDec(volatile int64_t& mem);
+
     };
 
 
     inline
     uint32_t
-    AtomicOperation::AtomicAdd32(volatile uint32_t& mem, uint32_t summand)
+    AtomicOperation::AtomicAdd(volatile uint32_t& mem, uint32_t summand)
     {
-        return os::arch::AtomicOperation::AtomicAdd32(mem, summand);
+        return os::arch::AtomicOperation::AtomicAdd(mem, summand);
+    }
+
+    inline
+    int32_t
+    AtomicOperation::AtomicAdd(volatile int32_t& mem, int32_t summand)
+    {
+        return os::arch::AtomicOperation::AtomicAdd(mem, summand);
+    }
+
+    inline
+    uint64_t
+    AtomicOperation::AtomicAdd(volatile uint64_t& mem, uint64_t summand)
+    {
+        return os::arch::AtomicOperation::AtomicAdd(mem, summand);
+    }
+
+    inline
+    int64_t
+    AtomicOperation::AtomicAdd(volatile int64_t& mem, int64_t summand)
+    {
+        return os::arch::AtomicOperation::AtomicAdd(mem, summand);
+    }
+
+
+    inline
+    uint32_t
+    AtomicOperation::AtomicSub(volatile uint32_t& mem, uint32_t subtrahend)
+    {
+        return os::arch::AtomicOperation::AtomicSub(mem, subtrahend);
+    }
+
+    inline
+    int32_t
+    AtomicOperation::AtomicSub(volatile int32_t& mem, int32_t subtrahend)
+    {
+        return os::arch::AtomicOperation::AtomicSub(mem, subtrahend);
+    }
+
+    inline
+    uint64_t
+    AtomicOperation::AtomicSub(volatile uint64_t& mem, uint64_t subtrahend)
+    {
+        return os::arch::AtomicOperation::AtomicSub(mem, subtrahend);
+    }
+
+    inline
+    int64_t
+    AtomicOperation::AtomicSub(volatile int64_t& mem, int64_t subtrahend)
+    {
+        return os::arch::AtomicOperation::AtomicSub(mem, subtrahend);
+    }
+
+
+    inline
+    uint32_t
+    AtomicOperation::AtomicInc(volatile uint32_t& mem)
+    {
+        return os::arch::AtomicOperation::AtomicInc(mem);
+    }
+
+    inline
+    int32_t
+    AtomicOperation::AtomicInc(volatile int32_t& mem)
+    {
+        return os::arch::AtomicOperation::AtomicInc(mem);
+    }
+
+    inline
+    uint64_t
+    AtomicOperation::AtomicInc(volatile uint64_t& mem)
+    {
+        return os::arch::AtomicOperation::AtomicInc(mem);
+    }
+
+    inline
+    int64_t
+    AtomicOperation::AtomicInc(volatile int64_t& mem)
+    {
+        return os::arch::AtomicOperation::AtomicInc(mem);
     }
 
     inline
     uint32_t
-    AtomicOperation::AtomicSub32(volatile uint32_t& mem, uint32_t subtrahend)
+    AtomicOperation::AtomicDec(volatile uint32_t& mem)
     {
-        return os::arch::AtomicOperation::AtomicSub32(mem, subtrahend);
+        return os::arch::AtomicOperation::AtomicDec(mem);
     }
 
     inline
-    uint32_t
-    AtomicOperation::AtomicInc32(volatile uint32_t& mem)
+    int32_t
+    AtomicOperation::AtomicDec(volatile int32_t& mem)
     {
-        return os::arch::AtomicOperation::AtomicInc32(mem);
+        return os::arch::AtomicOperation::AtomicDec(mem);
     }
 
     inline
-    uint32_t
-    AtomicOperation::AtomicDec32(volatile uint32_t& mem)
+    uint64_t
+    AtomicOperation::AtomicDec(volatile uint64_t& mem)
     {
-        return os::arch::AtomicOperation::AtomicDec32(mem);
+        return os::arch::AtomicOperation::AtomicDec(mem);
+    }
+
+    inline
+    int64_t
+    AtomicOperation::AtomicDec(volatile int64_t& mem)
+    {
+        return os::arch::AtomicOperation::AtomicDec(mem);
     }
 }
 
