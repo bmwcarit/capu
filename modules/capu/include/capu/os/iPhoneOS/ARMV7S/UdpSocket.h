3749e1dede9087b28365c028b8983c31d0094797
Aaron Frueh <aaron.frueh@partner.bmw.de>
2014-02-26 08:50:26 +0100

    Issue 32: Adding support for iPhoneOS and iPhoneSimulatorOS
    This patch adds support for both iPhoneOS and iPhoneSimulatorOS.
    
    For the iPhoneOS running on iOS device like the iPhone the following
    platforms are currently supported: ARMV7, ARMV7S, ARM64
    
    Furthermore the iPhone Simulator gets supported. Here the target
    platform x86 32 and x86 64 bit are available.
    
    Notice: It is not yet possible to build the GoogleTest framework for the
    iPhoneOS platforms.
    
    Change-Id: I6a17fb28c623f2ba35f82dd0eb60e9872f24477f

---
+++ modules/capu/include/capu/os/iPhoneOS/ARMV7S/UdpSocket.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2014 jambit GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CAPU_IPHONEOS_ARMV7S_UDP_SOCKET_H
+#define CAPU_IPHONEOS_ARMV7S_UDP_SOCKET_H
+
+#include <capu/os/iPhoneOS/UdpSocket.h>
+
+namespace capu
+{
+    namespace os
+    {
+        namespace arch
+        {
+            class UdpSocket: private capu::iphoneos::UdpSocket
+            {
+            public:
+                using capu::iphoneos::UdpSocket::bind;
+                using capu::iphoneos::UdpSocket::send;
+                using capu::iphoneos::UdpSocket::receive;
+                using capu::iphoneos::UdpSocket::close;
+                using capu::iphoneos::UdpSocket::setBufferSize;
+                using capu::iphoneos::UdpSocket::setTimeout;
+                using capu::iphoneos::UdpSocket::getBufferSize;
+                using capu::iphoneos::UdpSocket::getTimeout;
+                using capu::iphoneos::UdpSocket::getSocketAddrInfo;
+            };
+        }
+    }
+}
+
+#endif // CAPU_IPHONEOS_ARMV7S_UDP_SOCKET_H
---
+++ modules/capu/include/capu/os/iPhoneSimulatorOS/X86_32/Math.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2014 jambit GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CAPU_IPHONESIMULATOROS_X86_32_MATH_H
+#define CAPU_IPHONESIMULATOROS_X86_32_MATH_H
+
+#include <capu/os/iPhoneOS/Math.h>
+
+namespace capu
+{
+    namespace os
+    {
+        namespace arch
+        {
+            class Math: private capu::iphoneos::Math
+            {
+            public:
+                using capu::iphoneos::Math::PI_f;
+                using capu::iphoneos::Math::PI_d;
+                using capu::iphoneos::Math::LN2_f;
+                using capu::iphoneos::Math::LN2_d;
+                using capu::iphoneos::Math::Ceil;
+                using capu::iphoneos::Math::Floor;
+                using capu::iphoneos::Math::Abs;
+                using capu::iphoneos::Math::Sqrt;
+                using capu::iphoneos::Math::Pow2;
+                using capu::iphoneos::Math::Pow;
+                using capu::iphoneos::Math::Cos;
+                using capu::iphoneos::Math::Sin;
+                using capu::iphoneos::Math::Tan;
+                using capu::iphoneos::Math::ArcCos;
+                using capu::iphoneos::Math::ArcSin;
+                using capu::iphoneos::Math::ArcTan;
+                using capu::iphoneos::Math::Rad2Deg;
+                using capu::iphoneos::Math::Deg2Rad;
+                using capu::iphoneos::Math::Log2;
+                using capu::iphoneos::Math::Exp;
+            };
+        }
+    }
+}
+
+#endif
