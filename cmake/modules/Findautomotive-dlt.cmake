bd2fdcbd8d1abc14c71f0f97410d50c0e4b7ec59
Johannes Baeuerle <Johannes_Baeuerle@mentor.com>
2015-01-19 16:36:44 +0100

    Add cmake Find module for AUTOMOTIVE_DLT
    Change-Id: I6d1dab6c165449efecb8b356cbafd7713e4a77cc

---
+++ cmake/modules/Findautomotive-dlt.cmake
@@ -0,0 +1,15 @@
+SET(automotive-dlt_FOUND FALSE)
+
+# use package config to resolve dependencies,
+
+find_package(PkgConfig QUIET)
+
+IF(PKG_CONFIG_FOUND)
+    pkg_check_modules(automotive-dlt automotive-dlt)
+
+    MARK_AS_ADVANCED(
+        automotive-dlt_FOUND
+        automotive-dlt_INCLUDE_DIRS
+        automotive-dlt_LIBRARIES
+    )
+ENDIF()
