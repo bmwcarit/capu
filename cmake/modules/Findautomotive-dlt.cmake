410a115a750f6ad635ec5377a1803002e6f0f26d
Johannes Baeuerle <Johannes_Baeuerle@mentor.com>
2015-02-09 16:06:28 +0100

    Fix recognition of automotive-dlt for BuildConfig header
    Change-Id: I74bc1c8f79044b1004ea441722a337911afd894b

--- cmake/modules/Findautomotive-dlt.cmake
+++ cmake/modules/Findautomotive-dlt.cmake
@@ -1,15 +1,21 @@
-SET(automotive-dlt_FOUND 0)
-
 # use package config to resolve dependencies,
 
 find_package(PkgConfig QUIET)
 
 IF(PKG_CONFIG_FOUND)
     pkg_check_modules(automotive-dlt automotive-dlt)
-
+    IF(${automotive-dlt_FOUND})
+        SET(automotive-dlt_BUILD_CONFIG 1)
+    ELSE()
+        SET(automotive-dlt_BUILD_CONFIG 0)
+    ENDIF()
     MARK_AS_ADVANCED(
         automotive-dlt_FOUND
         automotive-dlt_INCLUDE_DIRS
         automotive-dlt_LIBRARIES
+        automotive-dlt_BUILD_CONFIG
     )
+ELSE()
+    SET(automotive-dlt_FOUND 0)
+    SET(automotive-dlt_BUILD_CONFIG 0)
 ENDIF()
